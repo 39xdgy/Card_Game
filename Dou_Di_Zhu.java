1f5bb2649cb8c3d767c166418812b2789a6049ae
39xdgy <wangjiashu3@126.com>
2018-02-08 21:41:42 -0600

    nothing

--- Dou_Di_Zhu.java
+++ Dou_Di_Zhu.java
@@ -1,9 +1,9 @@
-public class Dou_Di_Zhu {
+public class Dou_Di_Zhu{
 	private Player first,second,third;
 	private Deck cards;
 	private Card[] lead;
 	private boolean flag_startgame;
-	
+
 	public Dou_Di_Zhu(String first, String second, String third){
 		this.first = new Player(first);
 		this.second = new Player(second);
@@ -11,7 +11,7 @@ public class Dou_Di_Zhu {
 		cards = new Deck();
 		flag_startgame = false;
 	}
-	
+
 	public Dou_Di_Zhu(Player first, Player second, Player third){
 		this.first = first;
 		this.second = second;
@@ -19,13 +19,13 @@ public class Dou_Di_Zhu {
 		cards = new Deck();
 		flag_startgame = false;
 	}
-	
-	
+
+
 	public void gameStart(){
 		lead = cards.DDZdeal(first, second, third);
 		flag_startgame = true;
 	}
-	
-	
-	
+
+
+
 }
